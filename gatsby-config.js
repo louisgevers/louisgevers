12585271bd75034bc921b13361870bdcea57fdca
Louis Gevers <louis.gevers@gmail.com>
2020-05-04 15:31:01 +0200

    Create Home component and add dependencies for background images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,20 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const path = require(`path`)
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Home from "../components/home/home"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Louis Gevers</h1>
-    <p>Hi, this is my website</p>
+    <Home />
   </Layout>
 )
 
