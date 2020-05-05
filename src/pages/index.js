97e030a302e8844920ffd5a71f78d8448ce96d7c
Louis Gevers <louis.gevers@gmail.com>
2020-05-05 10:48:03 +0200

    Create Resume component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import Home from "../components/home/home"
 import About from "../components/about/about"
+import Resume from "../components/resume/resume"
 
 const IndexPage = () => (
   <Layout>
     <Home />
     <About />
+    <Resume />
   </Layout>
 )
 
