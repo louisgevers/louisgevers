9a5cfb7d4d8f3a9e70a5f348b396e6aab06f5af3
Louis Gevers <louis.gevers@gmail.com>
2020-05-06 12:22:07 +0200

    Complete About content

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -8,12 +8,26 @@ const About = () => (
   <ColoredSection title="about me" backgroundColor="#52796f" color="#ffffff">
     <TextParagraph>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat
-        velit dignissim sapien pulvinar, mollis molestie massa varius. Donec nec
-        sapien neque. Donec sagittis lacus in lorem tristique, in mattis sem
-        eleifend. Duis mauris nisi, volutpat non lacus vel, vestibulum commodo
-        nisi. Nullam vel elit convallis, viverra lacus non, scelerisque enim.
-        Mauris sit amet tristique arcu. Ut ut accumsan dolor.
+        I am a third year computer science bachelor student at Delft University
+        of Technology. Being an avid learner, I enjoy exploring more
+        technologies outside of my studies and using the skills I acquired to
+        work on personal projects. Well-versed in numerous programming languages
+        including Java, Python and JavaScript, I am well equiped to explore
+        different areas that are of interest to me, such as machine learning and
+        software engineering.
+      </p>
+      <p>
+        Although I am very comfortable developing apps on my own, I love working
+        with people. Familiar with Agile Development and Scrum, I value team
+        effort and effective communication. I followed public speaking courses
+        and had multiple teaching jobs, as I am a firm believer of the
+        importance of communicating one's ideas effectively.
+      </p>
+      <p>
+        Computer science aside, I like to spend my free time travelling to
+        various places and enjoying board sports (winter or summer), or calmer
+        hobbies like reading novels and drawing. I am also fairly interested in
+        psychology and anthropology, subjects I love discuss around a beer.
       </p>
     </TextParagraph>
     <FlagIcons />
